683c9f8990493650ffac7000d49286b681fbdb53
ekvf <maloplop7@gmail.com>
2024-10-24 19:26:27 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER["SERVER_NAME"] == '172.19.3.23') {
+    $dbhost = '172.19.3.23';
+    $dbuser = '';
+    $dbpassword = '';
+    $database = '';
+} else {
+    $dbhost = 'localhost';
+    $dbuser = 'lapshin.m.a';
+    $dbpassword = '8489';
+    $database = 'lapshin.m.a';
+}
+
